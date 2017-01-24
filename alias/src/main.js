bc540adea71d114a2c6b9a70aa596c197c78ad09
caiyanhu <caiyanhu752925240@qq.com>
2017-01-24 14:25:49 +0800

    doc:为路由重命名添加注释 弄清了相对路径与绝对路径的含义 - 如果children下的path没有/符号，表示相对路径，父路径是与children同级的路径内容 - 如果children下的path有/符号，表示绝对路径，其路径内容紧跟在localhost:8080后面 - 如果没有children选项（即嵌套路由），path内容必须是绝对路径（即有/符号），否则不知道这个相对路径相对于谁

--- alias/src/main.js
+++ alias/src/main.js
@@ -24,21 +24,21 @@ const router = new VueRouter({
     path: '/home',
     component: Home,
     children: [
-      // absolute alias:
+      // absolute alias: 因为alias前面有/，所以是绝对路径，浏览器访问的是localhost:8080/foo
       {
-        path: 'foo',
+        path: 'foo', // 因为foo前面没有/，所以是相对路径，这个路径对应的是localhost:8080/home/foo
         component: Foo,
         alias: '/foo'
       },
-      // relative alias (alias to /home/bar-alias)
+      // relative alias (alias to /home/bar-alias): 因为alias前面没有/，所以是相对于/home的路径（原因是bar path是/home path的children），浏览器访问的是localhost:8080/home/bar-alias
       {
-        path: 'bar',
+        path: 'bar', // 因为bar前面没有/，所以是相对路径，这个路径对应的是localhost:8080/home/bar
         component: Bar,
         alias: 'bar-alias'
       },
-      // multiple aliases
+      // multiple aliases。综上可得，浏览器分别访问的是localhost:8080/baz localhost:8080/home/baz-alias
       {
-        path: 'baz',
+        path: 'baz', // 因为baz前面没有/，所以是相对路径，这个路径对应的是localhost:8080/home/baz
         component: Baz,
         alias: ['/baz', 'baz-alias']
       }
