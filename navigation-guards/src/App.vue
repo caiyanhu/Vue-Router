d69f322d40ae0f71ef06a27599f676d92970fd54
caiyanhu <caiyanhu752925240@qq.com>
2017-01-24 15:22:27 +0800

    feat:增加导航钩子 参考Vue官网下路由文档 - 全局钩子 - 某个路由独享的钩子 - 组件内的钩子

---
+++ navigation-guards/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>Navigation Guards</h1>
+    <ul>
+      <li>
+        <router-link to="/">/</router-link>
+      </li>
+      <li>
+        <router-link to="/foo">/foo</router-link>
+      </li>
+      <li>
+        <router-link to="/bar">/bar</router-link>
+      </li>
+      <li>
+        <router-link to="/baz">/baz</router-link>
+      </li>
+      <li>
+        <router-link to="/qux">/qux</router-link>
+      </li>
+      <li>
+        <router-link to="/qux-async">/qux-async</router-link>
+      </li>
+    </ul>
+    <router-view></router-view>
+  </div>
+</template>
