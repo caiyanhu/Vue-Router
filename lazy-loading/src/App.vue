68506ac5d2726f58d255bb6b79799d0f3b2611f8
caiyanhu <caiyanhu752925240@qq.com>
2017-02-03 16:50:24 +0800

    feat:路由懒加载 将不同路由对应的组件分割成不同的代码块，然后当路由被访问的时候才加载对应的组件

---
+++ lazy-loading/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Basic</h1>
+    <ul>
+      <li>
+        <router-link to="/">/</router-link>
+      </li>
+      <li>
+        <router-link to="/foo">/foo</router-link>
+      </li>
+      <li>
+        <router-link to="/bar">/bar</router-link>
+      </li>
+      <li>
+        <router-link to="/baz">/baz</router-link>
+      </li>
+    </ul>
+    <router-view></router-view>
+  </div>
+</template>
---
+++ lazy-loading/src/Bar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>
+      {{ msg }}
+    </h2>
+    <p>I am lazy-loaded.(check out the Network tab in Chrome devtools.</p>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'This is Bar!'
+      }
+    }
+  }
+
+</script>
