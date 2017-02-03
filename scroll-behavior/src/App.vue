7670a4dad207ce95ea1074596bbebccefe74abde
caiyanhu <caiyanhu752925240@qq.com>
2017-02-03 16:52:09 +0800

    feat:滚动行为 当切换到新路由时，可以自定义路由切换时页面如何滚动。页面可以滚动到顶部，也可以保持原先的位置，就像重新加载页面那样。

---
+++ scroll-behavior/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Scroll Behavior</h1>
+    <ul>
+      <li>
+        <router-link to="/">/</router-link>
+      </li>
+      <li>
+        <router-link to="/foo">/foo</router-link>
+      </li>
+      <li>
+        <router-link to="/bar">/bar</router-link>
+      </li>
+    </ul>
+    <router-view></router-view>
+  </div>
+</template>
